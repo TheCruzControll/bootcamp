b7f42509fed87934a58c7c7e3c2596e3d35e2e3f
Francis N. Cruz <francisnikocruz@gmail.com>
2019-02-08 00:37:32 -0800

    finished todo project

--- 19-jQuery-todo/assets/js/todos.js
+++ 19-jQuery-todo/assets/js/todos.js
@@ -1,11 +1,14 @@
 //Check off specific todos by clicking
 $("ul").on("click","li",function(){
     $(this).toggleClass("completed");
+    $(this).children().eq(0).toggleClass("checked");
+    $(this).children().eq(2).children().toggleClass("confirm");
 });
 
-$("ul").on("click","span",function(event){
+
+$("ul").on("click",".trash",function(event){
     $(this).parent().fadeOut(200,function(){
-        $(this).remove();
+        $(this).parent().remove();
     });
     event.stopPropagation();
 });
@@ -15,6 +18,6 @@ $("input[type='text']").keypress(function(event){
         var todoText = $(this).val();
         $(this).val("")
         //create a new li and add to ul
-        $("ul").append("<li><span>O </span>"+todoText+"</li>")
+        $("ul").append("<li><span class='checkmark'><i class='far fa-check-circle'></i></span><input type='checkbox'/>"+todoText+"<span><i class='fas fa-times-circle trash'></i></span></li>")
     }
 })
